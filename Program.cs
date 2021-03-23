1200dd7017950bf945d6515e9883691702ff43ed
Hilal Yeşilova <hilal.yesilova0311@gmail.com>
2021-03-23 18:22:15 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myDic<int, string> myDict = new myDic<int, string>();
+
+            myDict.Add(123, "Hilal");
+            myDict.Add(1234, "Yusuf");
+            myDict.Add(1235, "Kenan");
+            myDict.Add(1236, "Gülce");
+
+            myDict.List();
+
+        }
+    }
+}
---
+++ myDic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myDictonary
+{
+    class myDic<TNum , TUser >
+    {
+        TNum[] num;
+        TUser[] user;
+
+        public myDic()
+        {
+            num = new TNum[0];
+            user = new TUser[0];
+        }
+
+        public void Add(TNum no , TUser name)
+        {
+            TNum[] tempNum = num;
+            TUser[] tempUser = user;
+
+            num = new TNum[num.Length + 1];
+            user = new TUser[user.Length + 1];
+
+            for (int i = 0; i < tempNum.Length; i++)
+            {
+                num[i] = tempNum[i];
+                user[i] = tempUser[i];
+
+            }
+
+            num[num.Length - 1] = no;
+            user[user.Length - 1] = name;
+
+        }
+                 
+        public void List()
+        {
+            for (int i = 0; i < user.Length; i++)
+            {
+                Console.WriteLine("Number : " + num[i] + " " + "Name :" + user[i]);
+            }
+        }
+
+        
+    }
+}
